c5ad82c0ef2a67880a23b8e0d6d8e5ffa0f685e3
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2025-05-24 13:31:44 -0300

    feat: Agregar nuevos componentes y funcionalidades para la gestión de cursos, exámenes finales y navegación, incluyendo ListWithTitle, BaseNavBar, Metadata, PageHeader, TrustedBrandsSection, y más.

---
+++ sistema-odin-project-app/src/views/Home/TrustedBrandsSection.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Title from "@/components/Title";
+import Subtitle from "@/components/Subtitle";
+
+export default function TrustedBrandsSection() {
+  const [brandImages, setBrandImages] = useState([]);
+
+  useEffect(() => {
+    const checkImages = async () => {
+      const loadedImages = [];
+      const totalImages = process.env.NEXT_PUBLIC_TRUSTED_BRANDS_QUANTITY;
+
+      for (let i = 1; i <= totalImages; i++) {
+        const imageUrl = `${process.env.NEXT_PUBLIC_TRUSTED_BRANDS_FILE_NAME}/brand${i}.png`;
+
+        const response = await fetch(imageUrl, { method: 'HEAD' });
+
+        if (response.ok) {
+          loadedImages.push(imageUrl);
+        }
+      }
+
+      setBrandImages(loadedImages);
+    };
+
+    checkImages();
+  }, []);
+
+  return (
+    <section className="p-4 bg-gray-100">
+      <Title
+        text="Empresas que ya confiaron en nosotros"
+        textPositionClass="text-center"
+        textColorClass="text-black"
+        customClasses="mb-4"
+      />
+
+      <Subtitle
+        text="Marcas que Avalan Nuestra Experiencia y Compromiso"
+        textPositionClass="text-center"
+        textColorClass="text-gray-700"
+        marginPositionClasses="mt-4 mb-8"
+        customClasses="text-lg"
+      />
+      <div className="flex flex-wrap justify-center gap-6">
+        {brandImages.length > 0 ? (
+          brandImages.map((image, index) => (
+            <div
+              key={index}
+              className="flex-shrink-0 w-32 h-32 flex items-center justify-center bg-white p-2 rounded-lg shadow-md"
+            >
+              <Image
+                src={image}
+                width={100}
+                height={100}
+                alt={`Marca ${index + 1}`}
+                className="object-contain"
+              />
+            </div>
+          ))
+        ) : (
+          <p className="text-center">No se encontraron imágenes.</p>
+        )}
+      </div>
+    </section>
+  );
+}
---
+++ sistema-odin-project-app/version.md
@@ -0,0 +1,15 @@
+1.1.3 - Nombre: Funcionalidad de ajustes de plataforma (numero de contacto y conexion a WhatsApp)
+|       Horas totales de desarrollo: 212
+|       Commit: 28e1cfaa121af35d9735546e89a0e7d5f7551c94
+|
+1.1.2 - Nombre: Funcionalidad de descripción por modulo de clase
+|       Horas totales de desarrollo: 206
+|       Commit: 369f15668127d8c3a94e126191e4f0dee1ba4e62
+|
+1.1.1 - Nombre: Transformacion de web app a web progressive app 
+|       Horas totales de desarrollo: 202
+|       Commit: 11db1f90def1a01ca4f86e7791cce36d8819e153
+|
+1.1.0 - Nombre: MVP
+        Horas totales de desarrollo: 200
+        Commit: 
